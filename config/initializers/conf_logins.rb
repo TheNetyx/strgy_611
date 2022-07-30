00ec02c1f6e30518ed15f9363e9060d00c49f3bf
_netyx <netyx0103@gmail.com>
2022-07-31 01:37:40 +0800

    add retarded way to store login credentials

---
+++ config/initializers/conf_logins.rb
@@ -0,0 +1,26 @@
+# this is a very stupid way to implement logins.
+class Logins
+    USERNAMES = [
+        "root",
+        "team1",
+        "team2",
+        "team3",
+        "team4",
+        "team5",
+        "team6",
+        "team7",
+        "team8",
+    ]
+
+    PASSWORDS = [
+        "supersecretadminpassword",
+        "149285",
+        "140872",
+        "495201",
+        "109873",
+        "087987",
+        "325974",
+        "349176",
+        "716987",
+    ]
+end
